ceadd3dcbb16f1fce53c1311aa658784ce7e4dd4
brown <brown2243@naver.com>
2021-09-08 21:07:41 +0900

    09/08

--- Programmers/Lv.2.js
+++ Programmers/Lv.2.js
@@ -26,3 +26,94 @@ function solution(words) {
     return words.split('').reduce((r, c, i) => r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1, 0);
 }
 //////////////////////////////
+
+// 위클리 챌린지 6주차
+// 복서 정렬하기
+// 정렬이 짜증나는 문제
+function solution(weights, head2head) {
+    const ans = [];
+    head2head.forEach((head, idx) => {
+        const tmp = head.split('')
+        const info = {}
+        info.weight = weights[idx]
+        info.idx = idx
+        info.winToBig = 0
+        info.battle = 0
+        info.win = 0
+        tmp.forEach((fight, i) => {
+            if (fight === 'N') { }
+            else if (fight === 'L') info.battle += 1
+            else if (fight === 'W') {
+                info.battle += 1
+                info.win += 1
+                if (info.weight < weights[i]) info.winToBig += 1
+            }
+        })
+        ans.push(info)
+    })
+    // 아래 내용 적용
+    return ans.sort((a, b) => {
+        if (a.win / a.battle > b.win / b.battle) return -1
+        if (a.win / a.battle < b.win / b.battle) return 1
+        if (a.winToBig > b.winToBig) return -1
+        if (a.winToBig < b.winToBig) return 1
+        if (a.weight > b.weight) return -1
+        if (a.weight < b.weight) return 1
+        if (a.idx < b.idx) return -1
+        if (a.idx > b.idx) return 1
+    }).map(({ idx }) => idx + 1)
+
+    return ans.sort((a, b) => {
+        if (a.win / a.battle > b.win / b.battle) {
+            return -1
+        } else if (a.win / a.battle < b.win / b.battle) {
+            return 1
+        } else {
+            if (a.winToBig > b.winToBig) {
+                return -1
+            } else if (a.winToBig < b.winToBig) {
+                return 1
+            } else {
+                if (a.weight > b.weight) {
+                    return -1
+                } else if (a.weight < b.weight) {
+                    return 1
+                } else {
+                    if (a.idx < b.idx) {
+                        return -1
+                    } else if (a.idx > b.idx) {
+                        return 1
+                    }
+                }
+            }
+        }
+    }).map(({ idx }) => idx + 1)
+}
+
+// 다른사람 풀이
+// 같은 방식인데 정렬만 이렇게 써도 훨씬 깔끔할 것 같다.
+function compareFunc(a, b) {
+    if (a.winRate < b.winRate) return 1;
+    if (a.winRate > b.winRate) return -1;
+    if (a.winToHeavy < b.winToHeavy) return 1;
+    if (a.winToHeavy > b.winToHeavy) return -1;
+    if (a.weight < b.weight) return 1;
+    if (a.weight > b.weight) return -1;
+    return a.idx - b.idx
+
+}
+answer = records.sort(compareFunc).map(v => v.idx)
+// 다른 방식
+function solution(weights, head2head) {
+    return head2head.map((l, i) => l.split('').reduce((acc, v, j) => {
+        acc[0] += v === 'W' ? 1 : 0;
+        acc[1] += v === 'W' ? weights[i] < weights[j] ? 1 : 0 : 0;
+        acc[2] += v === 'L' ? 1 : 0
+        return acc;
+    }, [0, 0, 0])
+    )
+        .map((v) => [v[0] / (v[0] + v[2]), v[1]])
+        .map((v, i) => [i + 1, { t: v[0], s: v[1], w: weights[i] }])
+        .sort((a, b) => b[1].t - a[1].t || b[1].s - a[1].s || b[1].w - a[1].w || a[0] - b[0])
+        .map((v) => v[0]);
+}
--- paper.js
+++ paper.js
@@ -1,4 +1,66 @@
-function solution(words) {
-    return words.split('').reduce((r, c, i) => r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1, 0);
+function solution(weights, head2head) {
+    const ans = [];
+    head2head.forEach((head, idx) => {
+        const tmp = head.split('')
+        const info = {}
+        info.weight = weights[idx]
+        info.idx = idx
+        info.winToBig = 0
+        info.battle = 0
+        info.win = 0
+        tmp.forEach((fight, i) => {
+            if (fight === 'N') { }
+            else if (fight === 'L') info.battle += 1
+            else if (fight === 'W') {
+                info.battle += 1
+                info.win += 1
+                if (info.weight < weights[i]) info.winToBig += 1
+            }
+        })
+        ans.push(info)
+    })
+    ans.sort((a, b) => {
+        if (a.win / a.battle > b.win / b.battle) {
+            return -1
+        } else if (a.win / a.battle < b.win / b.battle) {
+            return 1
+        } else {
+            if (a.winToBig > b.winToBig) {
+                return -1
+            } else if (a.winToBig < b.winToBig) {
+                return 1
+            } else {
+                if (a.weight > b.weight) {
+                    return -1
+                } else if (a.weight < b.weight) {
+                    return 1
+                } else {
+                    if (a.idx > b.idx) {
+                        return -1
+                    } else if (a.idx < b.idx) {
+                        return 1
+                    }
+                }
+            }
+        }
+    })
 }
-console.log(solution("A"))
+// var items = [
+//     { name: 'Edward', value: 21 },
+//     { name: 'Sharpe', value: 37 },
+//     { name: 'And', value: 45 },
+//     { name: 'The', value: -12 },
+//     { name: 'Magnetic', value: 13 },
+//     { name: 'Zeros', value: 37 }
+// ];
+// items.sort(function (a, b) {
+//     if (a.value > b.value) {
+//         return 1;
+//     }
+//     if (a.value < b.value) {
+//         return -1;
+//     }
+//     // a must be equal to b
+//     return 0;
+// });
+// console.log(items)
