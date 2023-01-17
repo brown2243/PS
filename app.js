8dd373f1a488586d1725e4026d14f19880792f12
brown <brown2243@naver.com>
2023-01-17 10:53:29 +0900

    PG: lv2 2문제

--- app.js
+++ app.js
@@ -1,50 +1,31 @@
-function solution(survey, choices) {
-  const MIND = [
-    ["R", "T"],
-    ["C", "F"],
-    ["J", "M"],
-    ["A", "N"],
-  ];
+function solution(storey) {
+  const arr = storey.toString().split("").map(Number);
+  let stone = 0;
 
-  const pointObj = MIND.reduce((acc, cur) => {
-    const [a, b] = cur;
-    acc[a] = 0;
-    acc[b] = 0;
-    return acc;
-  }, {});
-
-  for (let i = 0, N = survey.length; i < N; i++) {
-    const a = survey[i][0];
-    const b = survey[i][1];
-
-    const choice = choices[i];
-    if (choice === 4) {
-      continue;
+  for (let N = arr.length - 1, i = N; i >= 0; i--) {
+    const now = arr[i];
+    const next = arr[i - 1];
+    if (now > 5) {
+      stone += 10 - now;
+      if (next) {
+        arr[i - 1] += 1;
+      } else {
+        stone += 1;
+      }
     }
-    if (choice > 4) {
-      pointObj[b] += choice - 4;
-    } else {
-      pointObj[a] += 4 - choice;
+    if (now < 5) {
+      stone += now;
+    }
+    if (now === 5) {
+      if (next && next >= 5) {
+        stone += 10 - now;
+        arr[i - 1] += 1;
+      } else {
+        stone += now;
+      }
     }
   }
-
-  let ans = "";
-  MIND.forEach((val) => {
-    const [a, b] = val;
-    const aPoint = pointObj[a];
-    const bPoint = pointObj[b];
-    console.log(a, aPoint, b, bPoint);
-    if (aPoint > bPoint) {
-      ans += a;
-    }
-    if (aPoint < bPoint) {
-      ans += b;
-    }
-    if (aPoint === bPoint) {
-      ans += a.charCodeAt() - b.charCodeAt() > 0 ? b : a;
-    }
-  });
-
-  return ans;
+  return stone;
 }
-solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]);
+solution(16);
+solution(2554);
