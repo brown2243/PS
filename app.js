05a5c621e0e2503d9913f77b5bbc042154259e31
brown <brown2243@naver.com>
2023-01-25 23:08:11 +0900

    leetcode75: 3

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
-// 205. Isomorphic Strings
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-const isSubsequence = (s, t) => {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) {
-      i++;
-    }
-    j++;
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = (list1, list2) => {
+  const a = nodeToList(list1);
+  const b = nodeToList(list2);
+  let node;
+  a.concat(b)
+    .sort((a, b) => a - b)
+    .forEach((v) => {
+      if (!node) {
+        node = new ListNode(v);
+      } else {
+        node.next = new ListNode(v);
+      }
+    });
+  return node;
+};
+
+const nodeToList = (nodes) => {
+  const list = [];
+  while (nodes?.val) {
+    list.push(nodes.val);
+    nodes = nodes.next;
   }
-  return i === s.length;
+  return list;
+};
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var reverseList = (head) =>{
+  while(){
+      
+  }
 };
--- leetcode/LeetCode75.js
+++ leetcode/LeetCode75.js
@@ -138,3 +138,69 @@ var isSubsequence = (s, t) => {
   }
   return i === s.length;
 };
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let dummy = new ListNode(-1);
+  let current = dummy;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;
+  }
+
+  if (l1) current.next = l1;
+  if (l2) current.next = l2;
+
+  return dummy.next;
+}
+var mergeTwoLists = function (list1, list2) {
+  if (list1 === null) return list2;
+  if (list2 === null) return list1;
+
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+};
+
+var reverseList = (head) => {
+  let prev = null;
+  let current = head;
+  while (current) {
+    let nextNode = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextNode;
+  }
+  return prev;
+};
+var reverseList = (head) => {
+  let prev = null;
+  let next = null;
+  let current = head;
+
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
