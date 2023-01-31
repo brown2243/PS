ccceebaa47eb9c5f87d9e4c7a41ddf7a70813246
brown <brown2243@naver.com>
2023-01-31 21:20:34 +0900

    leetcode75: 7

--- app.js
+++ app.js
@@ -1,76 +1,67 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
  */
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
-var levelOrder = function (root, arr = [], depth = 1) {
-  if (!root) return [];
-  if (arr.length === 0) {
-    arr.push([root.val]);
-  }
-  const tmp = [];
-  tmp.push(root.left);
-  tmp.push(root.right);
-  const filteredTmp = tmp.filter((v) => v);
-  if (filteredTmp.length > 0) {
-    if (!arr[depth]) {
-      arr[depth] = [];
+var search = function (nums, target) {
+  let firstIdx = 0;
+  let lastIdx = nums.length - 1;
+  while (firstIdx <= lastIdx) {
+    const selectIdx = Math.floor((firstIdx + lastIdx) / 2);
+
+    if (nums[selectIdx] === target) {
+      return selectIdx;
+    }
+    if (nums[selectIdx] < target) {
+      firstIdx = selectIdx + 1;
+    } else {
+      lastIdx = selectIdx - 1;
     }
   }
-  for (const node of filteredTmp) {
-    arr[depth].push(node.val);
-  }
-  for (const node of filteredTmp) {
-    levelOrder(node, arr, depth + 1);
-  }
-  return arr;
+  return -1;
 };
 
-var levelOrder = function (root) {
-  const result = [];
+const ans = search([2, 5], 2);
+console.log(ans);
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 0;
+    let right = n;
+    let lastBad = null;
 
-  const bfs = (tree, level) => {
-    if (result.length === level) result.push([]);
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
 
-    result[level].push(tree.val);
-    if (tree.left) bfs(tree.left, level + 1);
-    if (tree.right) bfs(tree.right, level + 1);
+      if (isBadVersion(mid)) {
+        lastBad = mid;
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return lastBad;
   };
-
-  if (!root) return result;
-  bfs(root, 0);
-  return result;
 };
-var levelOrder = function (root) {
-  if (!root) return [];
 
-  const res = [];
-  const queue = [root];
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
 
-    for (let i = 0; i < levelSize; i++) {
-      const currentNode = queue.shift();
-
-      if (currentNode.left) {
-        queue.push(currentNode.left);
-      }
-      if (currentNode.right) {
-        queue.push(currentNode.right);
-      }
-      currentLevel.push(currentNode.val);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
-    res.push(currentLevel);
   }
 
-  return res;
+  return -1;
 };
--- leetcode/LeetCode75.js
+++ leetcode/LeetCode75.js
@@ -434,3 +434,46 @@ var levelOrder = function (root) {
 
   return res;
 };
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 0;
+    let right = n;
+    let lastBad = null;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (isBadVersion(mid)) {
+        lastBad = mid;
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return lastBad;
+  };
+};
