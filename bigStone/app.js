74915d9103c2d36370102fa2faa0ed0d7a1af36c
brown <brown2243@naver.com>
2023-05-01 16:52:26 +0900

    daliy: 04/27

--- bigStone/app.js
+++ bigStone/app.js
@@ -1,54 +1,16 @@
-// 19942
-{
-  const fs = require("fs");
-  const filePath =
-    process.platform === "linux" ? "/dev/stdin" : "bigStone/input.txt";
-  const input = fs.readFileSync(filePath).toString().trim();
-
-  const [[N], mins, ...arr] = input
-    .split("\n")
-    .map((v) => v.split(" ").map(Number));
-
-  // p, f, s, v
-  const [mp, mf, ms, mv] = mins;
-  let ans = Number.MAX_SAFE_INTEGER;
-  let ansArr = [];
-
-  for (let i = 1; i < 1 << N; i++) {
-    let b = 0,
-      c = 0,
-      d = 0,
-      e = 0,
-      sum = 0;
-
-    const tmp = [];
-
-    for (let j = 0; j < N; j++) {
-      if (i & (1 << j)) {
-        tmp.push(j + 1);
-        b += arr[j][0];
-        c += arr[j][1];
-        d += arr[j][2];
-        e += arr[j][3];
-        sum += arr[j][4];
-      }
+let n = 4;
+const a = ["사과", "딸기", "포도", "배"];
+for (let i = 0; i < 1 << n; i++) {
+  console.log("-------------");
+  console.log("-------------");
+  console.log("i:", i, i.toString(2).padStart(n, "0"));
+  console.log("-------------");
+  let ret = "";
+  for (let j = 0; j < n; j++) {
+    console.log("j:", j, j.toString(2).padStart(n, "0"));
+    if (i & (1 << j)) {
+      ret += a[j] + " ";
+      console.log(ret);
     }
-
-    if (b >= mp && c >= mf && d >= ms && e >= mv) {
-      if (ans >= sum) {
-        ans = sum;
-        if (!ansArr?.[ans]) {
-          ansArr[ans] = [];
-        }
-        ansArr[ans].push(tmp);
-      }
-    }
-  }
-
-  if (ans === Number.MAX_SAFE_INTEGER) {
-    console.log(-1);
-  } else {
-    ansArr[ans].sort();
-    console.log(`${ans}\n${ansArr[ans][0].join(" ")}`);
   }
 }
