16b4a056c2fa9e15e1d5de40cd24b40fcd4b6478
brown <brown2243@naver.com>
2023-06-19 21:17:09 +0900

    daliy: 06/19

--- backjon/app.js
+++ backjon/app.js
@@ -1,15 +1,31 @@
-// 큰 수 A+B
+// 소수
 {
   const fs = require("fs");
   const filePath =
     process.platform === "linux" ? "/dev/stdin" : "backjon/input.txt";
 
-  let [a, b] = fs
+  const [m, n] = fs
     .readFileSync(filePath)
     .toString()
     .trim()
-    .split(" ")
-    .map(BigInt);
+    .split("\n")
+    .map(Number);
 
-  console.log((a + b).toString());
+  const graph = new Array(n + 1).fill(true);
+  graph[0] = false;
+  graph[1] = false;
+  const primes = [];
+
+  for (let i = 2; i <= n; i++) {
+    if (graph[i]) {
+      primes.push(i);
+      for (let j = i * i; j <= n; j += i) {
+        graph[j] = false;
+      }
+    }
+  }
+  const idx = primes.findIndex((v) => m <= v);
+  const sliced = primes.slice(idx === -1 ? primes.length : idx);
+  const sum = sliced.reduce((acc, cur) => acc + cur, 0);
+  console.log(sliced.length === 0 ? -1 : `${sum}\n${sliced[0]}`);
 }
