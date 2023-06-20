3e91c91618dba3d175354fc306eb009ba88ebe85
brown <brown2243@naver.com>
2023-06-20 21:57:07 +0900

    daliy: 06/20

--- backjon/11.timeComplexity.js
+++ backjon/11.timeComplexity.js
@@ -68,3 +68,33 @@
   );
   console.log(3);
 }
+
+// 알고리즘 수업 - 점근적 표기 1
+{
+  const fs = require("fs");
+  const filePath =
+    process.platform === "linux" ? "/dev/stdin" : "backjon/input.txt";
+
+  const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+  const [a1, a0] = input[0].split(" ").map(Number);
+  const c = Number(input[1]);
+  const n = Number(input[2]);
+
+  let flag = true;
+  for (let i = n; i < 101; i++) {
+    const f = a1 * i + a0;
+    const g = c * i;
+
+    if (f > g) {
+      flag = false;
+      break;
+    }
+  }
+
+  if (flag) {
+    console.log(1);
+  } else {
+    console.log(0);
+  }
+}
--- backjon/app.js
+++ backjon/app.js
@@ -1,14 +1,29 @@
-// 알고리즘 수업 - 알고리즘의 수행 시간 6
+// 알고리즘 수업 - 점근적 표기 1
 {
   const fs = require("fs");
   const filePath =
     process.platform === "linux" ? "/dev/stdin" : "backjon/input.txt";
 
-  const input = fs.readFileSync(filePath).toString().trim();
-  const n = BigInt(input) - BigInt(2);
+  const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-  console.log(
-    ((n * n * n + BigInt(3) * n * n + BigInt(2) * n) / BigInt(6)).toString()
-  );
-  console.log(3);
+  const [a1, a0] = input[0].split(" ").map(Number);
+  const c = Number(input[1]);
+  const n = Number(input[2]);
+
+  let flag = true;
+  for (let i = n; i < 101; i++) {
+    const f = a1 * i + a0;
+    const g = c * i;
+
+    if (f > g) {
+      flag = false;
+      break;
+    }
+  }
+
+  if (flag) {
+    console.log(1);
+  } else {
+    console.log(0);
+  }
 }
