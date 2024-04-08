4479d5b988a18f0441c8376c0bf95258848c37d8
brown <brown2243@naver.com>
2024-04-08 10:38:28 +0900

    daliy: 04/08

--- backjon/app.js
+++ backjon/app.js
@@ -1,39 +1,24 @@
-// 11724
+// 13241
 {
   const fs = require("fs");
   const filePath =
     process.platform === "linux" ? "/dev/stdin" : "backjon/input.txt";
 
-  const [[N, M], ...arr] = fs
+  const arr = fs
     .readFileSync(filePath)
     .toString()
     .trim()
     .split("\n")
     .map((v) => v.split(" ").map(Number));
 
-  const n = N + 1;
-  const isVisted = new Array(n).fill(false);
-  const network = Array.from({ length: n }, () => []);
+  const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
 
-  arr.forEach(([x, y]) => {
-    network[x].push(y);
-    network[y].push(x);
-  });
-
-  let ans = 0;
-  const dfs = (now) => {
-    if (isVisted[now]) {
-      return;
-    }
-    isVisted[now] = true;
-    network[now].forEach(dfs);
-  };
-
-  for (let i = 1; i < n; i++) {
-    if (!isVisted[i]) {
-      dfs(i);
-      ans += 1;
-    }
-  }
-  console.log(ans);
+  console.log(
+    arr
+      .map(([a, b]) => {
+        const gcd = getGCD(a, b);
+        return gcd * (a / gcd) * (b / gcd);
+      })
+      .join("\n")
+  );
 }
--- backjon/history.js
+++ backjon/history.js
@@ -765,3 +765,53 @@
   }
   console.log(ans);
 }
+
+// 1934
+{
+  const fs = require("fs");
+  const filePath =
+    process.platform === "linux" ? "/dev/stdin" : "backjon/input.txt";
+
+  const [_, ...arr] = fs
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((v) => v.split(" ").map(Number));
+
+  const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
+
+  console.log(
+    arr
+      .map(([a, b]) => {
+        const gcd = getGCD(a, b);
+        return gcd * (a / gcd) * (b / gcd);
+      })
+      .join("\n")
+  );
+}
+
+// 13241
+{
+  const fs = require("fs");
+  const filePath =
+    process.platform === "linux" ? "/dev/stdin" : "backjon/input.txt";
+
+  const arr = fs
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((v) => v.split(" ").map(Number));
+
+  const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
+
+  console.log(
+    arr
+      .map(([a, b]) => {
+        const gcd = getGCD(a, b);
+        return gcd * (a / gcd) * (b / gcd);
+      })
+      .join("\n")
+  );
+}
